558212c4ecb0a47dd5f8fe1dc3c5a0759eba0d7b
Dustin Murdock <dmurdock2@gmail.com>
2020-07-09 23:55:11 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> anotherPlanetList = new List<string>() { "Uranus", "Neptune" };
+            planetList.AddRange(anotherPlanetList);
+            planetList.Insert(1, "Earth");
+            planetList.Insert(1, "Venus");
+            planetList.Add("Pluto");
+            List<string> rockyPlanets = planetList.GetRange(0 , 4);
+            planetList.Remove("Pluto");
+        }
+    }
+}
